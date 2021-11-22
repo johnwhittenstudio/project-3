c538eadd91d30d6cff0ad041863ebd225992050c
John Whitten <johnwhitten.studio@gmail.com>
2021-11-21 17:08:28 -0800

    clean up js

--- js/scripts.js
+++ js/scripts.js
@@ -1,21 +1,26 @@
 // Business Logic
 
 
-function beepBoop(number) {
-  // const numberArray = number.toString().split("");
-  if (isNaN(number)) {
-      return alert("Please enter a number from 1-100")
+function beepBoop(num) {
+  let numberArray = [];
+  for (let i = 0; i <= num; i++) {
+  if (i.toString().includes('3') === true) {
+    numberArray.push("Won't you be my neighbor?"); 
   }
-  if (number > 100) {
-    return alert("Please enter a number from 1-100")
-  }
-  for (let i = 0; i <=number; i++) {
-      if (i === 3 || i === 13 || i === 23 || i === 30 || i === 31 || i === 32 || i === 33 || i === 34 || i === 35 || i === 36 || i === 37 || i === 38 || i === 39 || i === 43  || i === 53  || i === 63  || i === 73  || i === 83 || i === 93) console.log("Won't you be my neighbor?");
-      else if (i === 1 || i === 10 || i === 11 || i === 14 || i === 15 || i === 16 || i === 17 || i === 18 || i === 19 || i === 41  || i === 51  || i === 61 || i === 71 || i === 81 || i === 91 || i === 100) console.log("Beep!");
-      else if (i === 2 || i === 12 || i === 20 || i === 21 || i === 22 || i === 23 || i === 24 || i === 25 || i === 26 || i === 27 || i === 28 || i === 29 || i === 42 || i === 52 || i === 62 || i === 72 || i === 82 || i === 92) console.log("Boop!");
-      else console.log(i);
-  }
-  };
+  else if  
+  (i.toString().includes('2') === true) {
+  numberArray.push("Boop!");
+} 
+else if 
+   (i.toString().includes('1') === true) {
+  numberArray.push("Beep!");
+}
+else { numberArray.push(i);
+}
+}
+  return numberArray;
+}
+beepBoop(34)
 
 
 // UI Logic
